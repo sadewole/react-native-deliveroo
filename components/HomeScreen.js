9f88414cd500c08630a84fd226e87a058e335dff
sadewole <samueladewole15@gmail.com>
2022-09-21 18:08:07 +0100

    feat: setup navigation and home screen

---
+++ components/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
